915b62a5a68a55ecdf5795c0eeced19a3ad050ca
amsv <ammosovaleksey@yandex.ru>
2023-03-10 01:54:29 +0300

    + страница 404 +роутинг

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,11 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import { AppRoute } from '../../const';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
 import MainScreen from '../../pages/main-screen/main-screen';
+import RoomScreen from '../../pages/room-screen/room-screen';
+import Page404 from '../../pages/page-404/page-404';
+
 
 const Setting = {
   CardsCount: 5,
@@ -6,7 +13,35 @@ const Setting = {
 
 function App() {
   return (
-    <MainScreen cardsCount = {Setting.CardsCount}/>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<MainScreen cardsCount = {Setting.CardsCount}/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<LoginScreen />}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={<FavoritesScreen />}
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<RoomScreen />}
+        />
+        <Route
+          path={AppRoute.Room}
+          element={<RoomScreen />}
+        />
+        <Route
+          path="*"
+          element={<Page404 />}
+        />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
