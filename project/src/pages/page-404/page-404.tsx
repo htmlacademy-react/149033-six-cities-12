7bccc485f128cf7a0983b534a03d529dcdfeed57
amsv <ammosovaleksey@yandex.ru>
2023-03-11 04:05:32 +0300

    +стили поключил через модули scss в page404 +svg разметку сделал в отдельный компонент

--- project/src/pages/page-404/page-404.tsx
+++ project/src/pages/page-404/page-404.tsx
@@ -1,38 +1,15 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-
+import Icon404 from '../../components/icon/icon404';
+import styles from './page-404.module.scss';
 function Page404() {
   return (
-    <div style = {{display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+    <div className={styles.wrapper}>
       <h1>404 Not Found</h1>
-      <Link to="/">Вернёмся на главную страницу!</Link>
-      <svg style={{maxWidth: '50%'}} xmlns='http://www.w3.org/2000/svg' viewBox='0 0 52 52'>
-        <g data-name="404-browser">
-          <g>
-            <path
-              fill="#eae8e8"
-              d="M4 50h44a2 2 0 002-2V12H2v36a2 2 0 002 2z"
-            />
-            <path
-              fill="#40495a"
-              d="M48 2H4a2 2 0 00-2 2v8h48V4a2 2 0 00-2-2z"
-            />
-            <path fill="#748191" d="M6 6H12V8H6z"></path>
-            <path fill="#748191" d="M16 6H46V8H16z"></path>
-          </g>
-          <path fill="#fff" d="M4 14H48V48H4z"></path>
-          <path fill="#80deea" d="M28 17H46V19H28z"></path>
-          <path fill="#80deea" d="M6 17H24V19H6z"></path>
-          <path fill="#26c6da" d="M25 17H27V19H25z"></path>
-          <path fill="#80deea" d="M29 43H47V45H29z"></path>
-          <path fill="#80deea" d="M6 43H24V45H6z"></path>
-          <path fill="#26c6da" d="M25 43H27V45H25z"></path>
-          <path
-            fill="#ff6e40"
-            d="M19.84 31.28H21v1.55h-1.16V35h-2.06v-2.17H13.5v-1.2L17.76 25h2.08zm-4.47 0h2.41v-3.64l-.21.45zM29.75 31.34a3.52 3.52 0 01-1 2.68A3.77 3.77 0 0126 35a3.81 3.81 0 01-2.73-1 3.52 3.52 0 01-1-2.68v-2.65a3.54 3.54 0 011-2.68A3.77 3.77 0 0126 25a3.79 3.79 0 012.72 1 3.53 3.53 0 011 2.68zm-2.22-2.88a2.28 2.28 0 00-.4-1.46 1.5 1.5 0 00-2.26 0 2.3 2.3 0 00-.4 1.45v3.06a2.32 2.32 0 00.4 1.46 1.37 1.37 0 001.13.53 1.35 1.35 0 001.13-.5 2.34 2.34 0 00.4-1.46zM37.34 31.28h1.16v1.55h-1.16V35h-2.06v-2.17H31v-1.2L35.26 25h2.08zm-4.47 0h2.41v-3.64l-.21.45z"
-          />
-        </g>
-      </svg>
+      <div className={styles.icon404}>
+        <Link to="/">Вернёмся на главную страницу!</Link>
+        <Icon404 />
+      </div>
     </div>
   );
 }
