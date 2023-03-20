189ba9aceedcaa25bde2cf5df4d1560995b3f147
amsv <ammosovaleksey@ya.ru>
2023-03-20 19:35:25 +0300

    +разбил тайпы

--- project/src/types/offers.ts
+++ project/src/types/offers.ts
@@ -1,30 +1,32 @@
+export type Location = {
+  latitude: number;
+  longitude: number;
+  zoom: number;
+}
+
+export type Host = {
+  id: number;
+  name: string;
+  isPro: boolean;
+  avatarUrl: string;
+}
+
+export type City = {
+  name: string;
+  location: Location;
+}
+
 export type Offer = {
   bedrooms: number;
-  city: {
-    location: {
-      latitude: number;
-      longitude: number;
-      zoom: number;
-    };
-    name: string;
-  };
+  city: City;
   description: string;
   goods: string[];
-  host: {
-    avatarUrl: string;
-    id: number;
-    isPro: boolean;
-    name: string;
-  };
+  host: Host;
   id: number;
   images: string[];
   isFavorite: boolean;
   isPremium: boolean;
-  location: {
-    latitude: number;
-    longitude: number;
-    zoom: number;
-  };
+  location: Location;
   maxAdults: number;
   previewImage: string;
   price: number;
