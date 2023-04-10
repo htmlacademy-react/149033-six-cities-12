7af5d7bcec0491686e9110ffb207ad8df007ab2e
amsv <ammosovaleksey@yandex.ru>
2023-04-10 23:23:44 +0300

    + убрал проверку на ошибку при старте index

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -5,7 +5,6 @@ import { reviews } from './mocks/reviews';
 import {Provider} from 'react-redux';
 import {store} from './store/index';
 import { checkAuthAction, fetchOfferAction } from './store/api-actions';
-import ErrorMessage from './components/error-message/error-message';
 
 store.dispatch(checkAuthAction());
 store.dispatch(fetchOfferAction());
@@ -17,7 +16,6 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <ErrorMessage />
       <App
         reviews={reviews}
       />
