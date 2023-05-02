113f4b7871919ba6cc3a0c35d5ffaf8e3aa7f71f
amsv <ammosovaleksey@yandex.ru>
2023-05-02 16:44:44 +0300

    +

--- project/src/components/bookmark-button/bookmark-button.tsx
+++ project/src/components/bookmark-button/bookmark-button.tsx
@@ -4,7 +4,6 @@ import { useAppDispatch, useAppSelector } from '../../hooks';
 import { setFavoritesAction } from '../../store/favorite-data/api-actions';
 import { getAuthCheckedStatus } from '../../store/user-process/selectors';
 import cn from 'classnames';
-import { useState } from 'react';
 
 type BookmarkButtonProps = {
   offerId: number;
@@ -16,7 +15,6 @@ export default function BookmarkButton({offerId, isFavorite, isBigSize}: Bookmar
   const dispatch = useAppDispatch();
   const isAuth = useAppSelector(getAuthCheckedStatus);
   const navigate = useNavigate();
-  const [isActive, setIsActive] = useState(isFavorite);
 
   const handleButtonClick = () => {
     if (isAuth) {
@@ -24,7 +22,6 @@ export default function BookmarkButton({offerId, isFavorite, isBigSize}: Bookmar
         id: offerId,
         status: Number(!isFavorite)
       }));
-      setIsActive( (option) => !option);
     } else {
       navigate(AppRoute.Login);
     }
@@ -33,9 +30,9 @@ export default function BookmarkButton({offerId, isFavorite, isBigSize}: Bookmar
     <button
       className={cn({
         'property__bookmark-button': isBigSize,
-        'property__bookmark-button--active': isBigSize && isActive,
+        'property__bookmark-button--active': isBigSize && isFavorite,
         'place-card__bookmark-button': !isBigSize,
-        'place-card__bookmark-button--active': !isBigSize && isActive,
+        'place-card__bookmark-button--active': !isBigSize && isFavorite,
       }, 'button')}
       type="button"
       onClick={handleButtonClick}
